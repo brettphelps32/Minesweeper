7cad44a326ae2d5c8d49289370d11364e50486b0
brettphelps32 <brettphelps32@gmail.com>
2023-12-19 16:27:33 -0600

    Add fuction to create the game board

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 import tkinter as tk
 from tkinter import *
 
-
-
-
-
-class GameGUI:
-    def __init__(self):
-        self.root = Tk()    
-
-        self.root.geometry("500x500")
-        self.root.title("MineSweeper")
-
 class MainGUI:
     def __init__(self):
 
@@ -38,7 +27,7 @@ class MainGUI:
         label2 = tk.Label(self.root, text="Number of bombs: ")
         label2.pack(pady = 40)
 
-        startButton = tk.Button(self.root, text="Start game", command=lambda: [self.startGame(), self.root.destroy])
+        startButton = tk.Button(self.root, text="Start game", command=lambda: [self.startGame()])
         startButton.pack(pady=50)
 
 
@@ -47,10 +36,24 @@ class MainGUI:
 
     def startGame(self):
         print(str(self.columnSize.get()) + "," + str(self.rowSize.get()))
+        self.root.destroy()
         self.createGame(self.columnSize.get(),self.rowSize.get())
+        # GameGUI(self.columnSize.get, self.rowSize.get())
+
 
     def createGame(self, col, row):
-        print("Starting Game")
+        self.game = Tk()
+        geo = "%dx%d" % (col *40 , row*40)
+        self.game.geometry(geo)
+
+        self.game.title("MineSweeper")
+        mineGrid = tk.Frame(self.game)
+        for i in range(col):
+            for j in range(row):
+                mineGrid.columnconfigure(i, weight=1)
+                btn = tk.Button(mineGrid, text="      ")
+                btn.grid(row=j, column=i)
+        mineGrid.pack()
 
     
 
